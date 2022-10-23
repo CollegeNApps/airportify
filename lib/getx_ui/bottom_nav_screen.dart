ecc8345f9e4203423ff33da19d366c07994f9b7d
Angularsight <vishnup@summorfoods.in>
2022-10-23 18:20:09 +0530

    Created two new branches again and again

--- lib/getx_ui/bottom_nav_screen.dart
+++ lib/getx_ui/bottom_nav_screen.dart
@@ -8,7 +8,7 @@ class BottomNavigationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Whatever"),
+      body: Text("ajg;kljadsf"),
     );
   }
 }
