6f729c2e616e2082b164ada8cbac535046d73122
vishwaskotegar <vishwas.skotegar.is@gmailcom>
2022-11-04 11:24:21 +0530

    4/11/ 22 commit 1 (minor change) - Structured flight model

--- lib/controllers/firebase_controller.dart
+++ lib/controllers/firebase_controller.dart
@@ -1,24 +1,19 @@
-
-
 import 'dart:ui';
 
-import 'package:airportify/models/flight_info.dart';
+import 'package:airportify/models/flight/flight_info.dart';
 import 'package:airportify/models/user/user_info.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class FirebaseController extends GetxController{
-
-
+class FirebaseController extends GetxController {
   final _flights = [FlightInfo()].obs;
   List<FlightInfo> get flights => _flights.value;
-
 
   RxInt journeyType = 0.obs;
   RxInt serviceType = 0.obs;
 
-  final flightStatusCodeColors =  const[
+  final flightStatusCodeColors = const [
     Color(0xff7ad95b),
     Color(0xffffbb3e),
     Color(0xff7f7f7f),
@@ -31,20 +26,26 @@ class FirebaseController extends GetxController{
     _flights.bindStream(connectFlightStream());
   }
 
-  Stream<List<FlightInfo>> connectFlightStream(){
-    var result = FirebaseFirestore.instance.collection('flights').snapshots().map((query) {
+  Stream<List<FlightInfo>> connectFlightStream() {
+    var result = FirebaseFirestore.instance
+        .collection('flights')
+        .snapshots()
+        .map((query) {
       var docs = query.docs.map((e) => FlightInfo.fromDocument(e)).toList();
       return docs;
     });
     return result;
   }
-  
-  static Future<UserDetails> fetchUserInfo(User user)async{
-    var userResult = await FirebaseFirestore.instance.collection('users').where('uid',isEqualTo: user.uid).get().then((query) {
+
+  static Future<UserDetails> fetchUserInfo(User user) async {
+    var userResult = await FirebaseFirestore.instance
+        .collection('users')
+        .where('uid', isEqualTo: user.uid)
+        .get()
+        .then((query) {
       var docs = query.docs.map((e) => UserDetails.fromDocument(e)).toList();
       return docs[0];
     });
     return userResult;
   }
-
 }
