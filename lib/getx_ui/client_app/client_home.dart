1b3668bb647c5ca18b2185b6f1c7ee2a263ca6c0
vishwaskotegar <vishwas.skotegar.is@gmailcom>
2022-11-04 11:39:10 +0530

    4/11/22 commit 2(minor change) - 1. renamed client home 2. fixed inkwell bug

---
+++ lib/getx_ui/client_app/client_home.dart
@@ -0,0 +1,265 @@
+import 'package:airportify/controllers/auth_controller.dart';
+import 'package:airportify/controllers/firebase_controller.dart';
+import 'package:airportify/getx_ui/client_app/flight_info.dart';
+import 'package:airportify/models/flight/flight_info.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ClientHomeScreen extends StatelessWidget {
+  ClientHomeScreen({Key? key}) : super(key: key);
+
+  final FirebaseController fb = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    var size = MediaQuery.of(context).size;
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+    final user = AuthController.firebaseUser;
+
+    return Scaffold(
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              floating: true,
+              automaticallyImplyLeading: false,
+              // centerTitle: true,
+              title: Text(
+                "Hi ${user.username}",
+                style: textTheme.headline1!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w500),
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () => null,
+                    icon: const Icon(Icons.notifications_outlined,
+                        color: Colors.black)),
+                IconButton(
+                    onPressed: () => null,
+                    icon:
+                        const Icon(Icons.account_circle, color: Colors.black)),
+              ],
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                  height: size.height * 0.32,
+                  child: Image.asset(
+                    "images/airport image.gif",
+                    fit: BoxFit.fitHeight,
+                  )),
+            ),
+          ];
+        },
+        body: PhysicalModel(
+          color: Colors.transparent,
+          elevation: 5,
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Flights",
+                      style: textTheme.headline1!
+                          .copyWith(fontSize: 24, fontWeight: FontWeight.w500),
+                    ),
+                    GetX<FirebaseController>(builder: (ctrl) {
+                      return Expanded(
+                        child: SizedBox(
+                          child: ScrollConfiguration(
+                            behavior: MyBehavior(),
+                            child: ListView.builder(
+                              padding: EdgeInsets.zero,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: ctrl.flights.length,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                FlightInfo flight = ctrl.flights[index];
+
+                                return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8, horizontal: 4),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.black
+                                                    .withOpacity(0.5),
+                                                offset: const Offset(0, 1),
+                                                blurRadius: 2,
+                                                spreadRadius: 0)
+                                          ],
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Material(
+                                        color: Colors.transparent,
+                                        child: InkWell(
+                                          onTap: () {
+                                            Get.to(() => FlightInfoScreen(
+                                                  index: index,
+                                                ));
+                                          },
+                                          child: SizedBox(
+                                            width: w,
+                                            height: h * 0.11,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(3),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                children: [
+                                                  Hero(
+                                                    tag: 'flightImage$index',
+                                                    child: ClipOval(
+                                                      child: CircleAvatar(
+                                                        radius: w * 0.1,
+                                                        backgroundColor:
+                                                            Colors.white,
+                                                        child: Image.network(
+                                                          flight.image
+                                                              .toString(),
+                                                          fit: BoxFit.contain,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Text(
+                                                        flight.name.toString(),
+                                                        style: textTheme
+                                                            .headline1!
+                                                            .copyWith(
+                                                                fontSize: 15),
+                                                      ),
+                                                      const SizedBox(
+                                                        height: 5,
+                                                      ),
+                                                      Text(
+                                                        "Arrival Time : ${flight.at}",
+                                                        style: textTheme
+                                                            .headline1!
+                                                            .copyWith(
+                                                                fontSize: 12,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .normal),
+                                                      ),
+                                                      // SizedBox(width: w*0.08,),
+                                                      const SizedBox(
+                                                        height: 5,
+                                                      ),
+                                                      Text(
+                                                        "From : ${flight.from}",
+                                                        style: textTheme
+                                                            .headline1!
+                                                            .copyWith(
+                                                                fontSize: 12,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .normal),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        const EdgeInsets.only(
+                                                            right: 5),
+                                                    child: Column(
+                                                      children: [
+                                                        Text(
+                                                          '',
+                                                          style: textTheme
+                                                              .headline1!
+                                                              .copyWith(
+                                                                  fontSize: 15),
+                                                        ),
+                                                        const SizedBox(
+                                                          height: 5,
+                                                        ),
+                                                        Text(
+                                                          "Airline : ${flight.airline}",
+                                                          style: textTheme
+                                                              .headline1!
+                                                              .copyWith(
+                                                                  fontSize: 12,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .normal),
+                                                        ),
+                                                        const SizedBox(
+                                                          height: 5,
+                                                        ),
+                                                        Container(
+                                                          width: w * 0.23,
+                                                          height: 20,
+                                                          decoration: BoxDecoration(
+                                                              color: ctrl
+                                                                  .flightStatusCodeColors[flight
+                                                                      .statusCode!
+                                                                      .toInt() -
+                                                                  1],
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          4)),
+                                                          child: Center(
+                                                            child: Text(
+                                                              "${flight.status}",
+                                                              style: textTheme
+                                                                  .headline1!
+                                                                  .copyWith(
+                                                                      fontSize:
+                                                                          12),
+                                                            ),
+                                                          ),
+                                                        )
+                                                      ],
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ));
+                              },
+                            ),
+                          ),
+                        ),
+                      );
+                    })
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/getx_ui/client_app/confirmation_screen.dart
+++ lib/getx_ui/client_app/confirmation_screen.dart
@@ -1,7 +1,4 @@
-
-
-
-import 'package:airportify/getx_ui/client_app/home_screen.dart';
+import 'package:airportify/getx_ui/client_app/client_home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
@@ -18,36 +15,49 @@ class ConfirmationScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(height:h*0.4,),
+          SizedBox(
+            height: h * 0.4,
+          ),
           Center(
             child: CircleAvatar(
-                radius: w*0.2,
+                radius: w * 0.2,
                 backgroundColor: Theme.of(context).primaryColor,
                 child: Lottie.asset('images/lottie/lf30_editor_0imp56vx.json')),
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           Center(
-            child: Text("Booking Confirmed",style: Theme.of(context).textTheme.headline1!.copyWith(
-              fontSize: 25,
-              fontWeight: FontWeight.bold
-            ),),
+            child: Text(
+              "Booking Confirmed",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline1!
+                  .copyWith(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
           ),
           const Spacer(),
           Center(
             child: InkWell(
-              onTap: ()=> Get.offAll(()=> HomeScreen()),
+              onTap: () => Get.offAll(() => ClientHomeScreen()),
               child: Container(
-                  width: w*0.5,
+                  width: w * 0.5,
                   height: 35,
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: const Center(child: Text("Back to Home",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),)
-              ),
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Center(
+                    child: Text(
+                      "Back to Home",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black),
+                    ),
+                  )),
             ),
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
         ],
       ),
     );
